40d9a5911fc74b163e5d5fe019ec74976e2ec706
Efi Rahamim <efirahamim@gmail.com>
2023-12-06 15:16:25 +0200

    open new script: analyzePsiSigma results

---
+++ AnalyzeTranscripts_PSI-Sigma.py
@@ -0,0 +1,4 @@
+# Steps: 
+# 1. Run over PSI-Sigma filtered results. 
+# 2. For each splicing event: (1) find the sequence of the alternative exon (by Target.Exon field)
+#                             (2) find the inclusion and exclusion sequences 
