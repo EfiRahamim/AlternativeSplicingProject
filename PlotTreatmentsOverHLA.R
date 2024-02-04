b3834b1c8e6c79592529e0e83c5ffed82eff460a
Efi Rahamim <efirahamim@gmail.com>
2024-02-04 13:52:43 +0200

    change lables of groups

--- PlotTreatmentsOverHLA.R
+++ PlotTreatmentsOverHLA.R
@@ -1,10 +1,11 @@
 library(tidyverse)
-data <- read.csv("/private10/Projects/Efi/AML/rMATS/netMHCcombined.csv")
+data <- read.csv("/private10/Projects/Efi/AML/NeoEpitopesAnalyze/netMHCcombined.csv")
 data_long <- data %>%
   pivot_longer(cols = starts_with("HLA"), names_to = "HLA", values_to = "StrongBinders") #%>%
   #pivot_longer(cols = c("SplicingType", "Group"), names_to = "Category", values_to = "Value")
-treatments_desired_order <- c("SF mutations", "Mock_6h", "Indisulam", "Pladienolide-B", "Mock_18h", "5-Azacytidine", "FB23-2")
-data_long$Group <- factor(data_long$Group, levels = desired_order)
+levels(data_long$Group)[1] <- 'NoTreatment' # change 'SF Mutation' group into 'NoTreatment'
+treatments_desired_order <- c("NoTreatment", "Mock_6h", "Indisulam", "Pladienolide-B", "Mock_18h", "5-Azacytidine", "FB23-2")
+data_long$Group <- factor(data_long$Group, levels = treatments_desired_order)
 # plot distribution of AS type in each treatment for each HLA allel
 plot <- ggplot(data_long, aes(x = Group, y = StrongBinders, fill = SplicingType)) +
   geom_bar(stat = "identity", position = "stack") +
